e35a03a8a87c9160906ccfd71d877004c4edf61c
Dragnum <dragnumexp@gmail.com>
2025-12-02 07:32:12 -0300

    Finished week 5 assignment

--- controllers/users.js
+++ controllers/users.js
@@ -2,74 +2,102 @@ const { response } = require("express");
 const mongodb = require("../data/database");
 const ObjectId = require("mongodb").ObjectId;
 
-const getAll = async(req, res) => {
-    //#swagger.tags = ['Users']
-    const db = mongodb.getDatabase();
-    const usersCollection = db.db().collection('users');
+const getAll = async (req, res) => {
+  //#swagger.tags=["Users"]
+  try {
+    const lists = await mongodb
+      .getDatabase()
+      .db("CarShop")
+      .collection("users")
+      .find()
+      .toArray();
 
-    usersCollection.find().toArray()
-        .then((users) => {
-            res.status(200).json(users);
-        })
-        .catch((err) => {
-            res.status(500).json({ message: 'Fetching users failed.' });
-        });
+    res.status(200).json(lists);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
+
 const getSingle = async (req, res) => {
-    //#swagger.tags = ['Users']
+  //#swagger.tags=["Users"]
+
+  if (!ObjectId.isValid(req.params.id)) {
+    return res.status(400).json({ message: 'Must use a valid user id to find a contact.' });
+  }
+
+  try {
     const userId = new ObjectId(req.params.id);
-    const result = await mongodb.getDatabase().collection("users").findOne({_id: userId});
-    result.toArray().then((user) => {
-        res.setHeader("Content-Type", "application/json");
-        res.status(200).json(users[0]);
-    }).catch((err) => {
-        res.status(500).json({message: "An error occurred", error: err});
-    });
+
+    const result = await mongodb
+      .getDatabase()
+      .db("CarShop")
+      .collection("users")
+      .findOne({ _id: userId });
+
+    if (!result) {
+      return res.status(404).json({ message: 'User not found.' });
+    }
+
+    res.status(200).json(result);
+
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 const createUser = async (req, res) => {
-    //#swagger.tags = ['Users']
-    const user  = {
-        email: req.body.email,
-        username: req.body.username,
-        name: req.body.name,
-        ipaddress: req.body.ipaddress,
-    };
-    const response = await mongodb.getDatabase().db().collection('users').insertOne(user);
-    if (response.acknowledge) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while creating the user.');
-    }
-};
-
-const updateUser = async (req, res) => {
-    //#swagger.tags = ['Users']
-    const userId = new ObjectId(req.params.id);
+  //#swagger.tags=["Users"]
     const user = {
         email: req.body.email,
         username: req.body.username,
         name: req.body.name,
         ipaddress: req.body.ipaddress,
     };
-    const response = await mongodb.getDatabase().db().collection('users').replaceOne({ _id: userId }, (user));
-    if (response.modifiedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while updating the user.');
+    try{
+      const response = await mongodb.getDatabase().db("CarShop").collection("users").insertOne(user);
+      if (response.acknowledged > 0) {
+        res.status(204).send()
+      } 
+    }catch(err) {
+        res.status(500).json({
+      message: err.message || "Some error occurred while creating the User"});
+      }
+};
+
+const updateUser = async (req, res) => {
+  //#swagger.tags=["Users"]
+    const userId = new ObjectId(req.params.id);
+    const user = {
+        email: req.body.email,
+        username: req.body.username,
+        name: req.body.name,
+        ipaddress: req.body.ipaddress,
+
+    };
+    try{
+      const response = await mongodb.getDatabase().db("CarShop").collection("users").replaceOne({ _id: userId}, user);
+      if (response.modifiedCount > 0) {
+        res.status(204).send()
+      } 
+    }catch(err) {
+        res.status(500).json({
+        message: err.message || "Some error occurred while updating the User"});
     }
 };
 
 const deleteUser = async (req, res) => {
-    //#swagger.tags = ['Users']
+  //#swagger.tags=["Users"]
     const userId = new ObjectId(req.params.id);
-    const response = await mongodb.getDatabase().db().collection('users').deleteOne({ _id: userId });
+    try{
+    const response = await mongodb.getDatabase().db("CarShop").collection("users").deleteOne({ _id: userId});
     if (response.deletedCount > 0) {
-        res.status(204).send();
-    } else {
-        res.status(500).json(response.error || 'Some error occurred while deleting the user.');
-    }
+      res.status(204).send()
+    } 
+  }catch(err) {
+      res.status(500).json({
+      message: err.message || "Some error occurred while deleting the User"});
+  }
 };
 
 module.exports = { getAll, getSingle, createUser, updateUser, deleteUser };
